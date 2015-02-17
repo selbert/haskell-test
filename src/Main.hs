3fea925bcd7c3cb5936696ac35c6981cf51936e6
Thomas <selbert87@gmail.com>
2015-02-17 04:50:17 -0800

    async console read/write

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,25 @@
 module Main where
 
-import Control.Concurrent
+import           Control.Concurrent
 
 main :: IO ()
 main = do
-    forkIO $ do
-        a <- getLine
-        putStrLn a
-    putStrLn "Have a good day!"
+    chan <- newChan
+    _ <- forkIO $ readConsoleInput chan
+    printOutputToConsole chan
+    return ()
+
+readConsoleInput :: Chan String -> IO ()
+readConsoleInput chan = do
+    _ <- forkIO $ forever $ do
+        line <- getLine
+        writeChan chan line
+    return ()
+    where
+        forever a = a >> forever a
+
+printOutputToConsole :: Chan String -> IO ()
+printOutputToConsole chan = do
+    line <- readChan chan
+    putStrLn line
+    printOutputToConsole chan
